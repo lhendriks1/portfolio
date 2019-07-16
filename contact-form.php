5961e11d0140b8374dd3e4b2d7593e10644a388e
Lydia Hendriks <hendriks.lydia@gmail.com>
2019-07-16 12:01:32 -0400

    updated php

--- contact-form.php
+++ contact-form.php
@@ -1,38 +1,80 @@
 <?php
-// Fetching Values from URL.
-$name = $_POST['name1'];
-$email = $_POST['email1'];
-$message = $_POST['message1'];
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
-// After sanitization Validation is performed
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-if (!preg_match("/^[0-9]{10}$/", $contact)) {
-echo "<span>* Please Fill Valid Contact No. *</span>";
-}
-else {
-$subject = $name;
-// To send HTML mail, the Content-type header must be set.
-$headers = 'MIME-Version: 1.0' . "rn";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "rn";
-$headers .= 'From:' . $email. "rn"; // Sender's Email
-$headers .= 'Cc:' . $email. "rn"; // Carbon copy to Sender
-$template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
-. '<br/>Thank you...! For Contacting Us.<br/><br/>'
-. 'Name:' . $name . '<br/>'
-. 'Email:' . $email . '<br/>'
-. 'Contact No:' . $contact . '<br/>'
-. 'Message:' . $message . '<br/><br/>'
-. 'This is a Contact Confirmation mail.'
-. '<br/>'
-. 'We Will contact You as soon as possible .</div>';
-$sendmessage = "<div style="background-color:#7E7E7E; color:white;">" . $template . "</div>";
-// Message lines should not exceed 70 characters (PHP rule), so wrap it.
-$sendmessage = wordwrap($sendmessage, 70);
-// Send mail by PHP Mail Function.
-mail("receiver_email_id@abc.com", $subject, $sendmessage, $headers);
-echo "Your Query has been received, We will contact you soon.";
-}
-} else {
-echo "<span>* invalid email *</span>";
+if(isset($_POST['email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "hendriks.lydia@gmail.com";
+    $email_subject = "Form Submission from site";
+
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+
+
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+
+
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+
+  if(strlen($message) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+
+
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+<!-- include your own success html here -->
+
+Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
+
 }
 ?>
